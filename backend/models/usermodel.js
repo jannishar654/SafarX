c1d56c4f9f67376ed8dab51551e4879c03afa73c
Jannishar <mdjannishar654@gmail.com>
2026-01-03 23:06:40 +0530

    User Model setup

---
+++ backend/models/usermodel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose'); 
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        firstname: {
+            type: String, 
+            required: true, 
+            minlength: [3, 'First name must be at least 3 characters long'],
+
+        }
+    }
+
+})
