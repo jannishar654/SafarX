b4d3cdb8893894435b264d495be56b381e96adad
Jannishar <mdjannishar654@gmail.com>
2026-01-20 18:30:40 +0530

    Fix user registration validation and routes

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,26 @@
+const express = require('express'); 
+const router = express.Router();
+const { body } = require("express-validator"); 
+const userController = require('../controllers/user.controller'); 
+
+router.post('/register',[
+    body('email').isEmail().withMessage('Invalid Email'), 
+    body('fullname.firstname').isLength({ min:3}).withMessage
+    ('Fisrt name must be at least 3 charcaters long'), 
+    body('password').isLength({ min:6}).withMessage
+    ('Password must be at least 6 charcaters long'), 
+
+
+
+
+],
+userController.registerUser // passing as a controller 
+)
+
+
+
+
+
+
+module.exports = router; 
+
