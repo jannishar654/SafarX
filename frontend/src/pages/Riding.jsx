8a4bf157293d10f781edeb93f75d15f65098e97d
Jannishar <mdjannishar654@gmail.com>
2026-01-30 16:50:02 +0530

    User Live Riding Page added

---
+++ frontend/src/pages/Riding.jsx
@@ -0,0 +1,56 @@
+import React  from 'react'
+import { Link } from 'react-router-dom'
+
+const Riding = () => {
+    return (
+        <div className='h-screen'>
+            <Link to='/home' className='fixed right-2 top-2 h-10 w-4 bg-white flex items-center justify-center rounded-full '>
+                <i className=' text-lg font-medium ri-home-5-line ' ></i>
+            </Link>
+
+            <div className="h-1/2">
+                <img className='h-full w-full object-cover ' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="Home Page" />
+
+            </div>
+
+            <div className="h-1/2 p-4">
+                <div className='flex items-center justify-between '>
+                    <img className="h-13" src="https://cn-geo1.uber.com/image-proc/crop/resizecrop/udam/format=auto/width=956/height=538/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC91ZGFtLWFzc2V0cy85MDM0YzIwMC1jZTI5LTQ5ZjEtYmYzNS1lOWQyNTBlODIxN2EucG5n" alt="Car" />
+                    <div className='text-right'>
+                        <h2 className='text-lg font-md '> Jannishar</h2>
+                        <h4 className='text-xl font-semibold -mt-1 -mb-1'> BR05A 123</h4>
+                        <p className='text-sm text-gray-600'>BMW Alto </p>
+                    </div>
+                </div>
+
+                <div className="flex  gap-2 justify-between flex-col items-center">
+
+                </div>
+
+                <div className="w-full mt-5">
+                    
+                    <div className='flex items-center gap-5 p-3 border-b-2 border-gray-100 '>
+                        <i className="text-lg ri-map-pin-2-fill"></i>
+                        <div>
+                            <h3 className='text-lg font-md'> 25C/12-B</h3>
+                            <p className='text-sm -mt-1 text-gray-600'> Jamia Millia Islamia </p>
+
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-5 p-3  '>
+                        <i className="text-lg ri-currency-line"></i>
+                        <div>
+                            <h3 className='text-lg font-md'> Rs 201.05</h3>
+                            <p className='text-sm -mt-1 text-gray-600'> Cash - Cash </p>
+
+                        </div>
+                    </div>
+                </div>
+                <button className= "w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg ">Make a Payment </button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Riding
