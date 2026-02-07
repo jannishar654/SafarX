0f36a5821d4f60f80f523fcc833848aa2bfe9291
Jannishar <mdjannishar654@gmail.com>
2026-02-07 16:34:21 +0530

    Socket error fixed

--- backend/socket.js
+++ backend/socket.js
@@ -5,58 +5,74 @@ const captainModel = require('./models/captain.model');
 let io;
 
 function initializeSocket(server) {
-    io = socketIo(server, {
-        cors: {
-            origin: '*',
-            methods: [ 'GET', 'POST' ]
+  io = socketIo(server, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST']
+    }
+  });
+
+  io.on('connection', (socket) => {
+    console.log(`Client connected: ${socket.id}`);
+
+   
+    socket.on('join', async (data) => {
+      const { userId, userType } = data || {};
+
+      if (!userId) {
+        console.log('join skipped: userId missing');
+        return;
+      }
+
+      if (userType === 'user') {
+        await userModel.findByIdAndUpdate(
+          userId,
+          { socketId: socket.id }
+        );
+      } 
+      else if (userType === 'captain') {
+        await captainModel.findByIdAndUpdate(
+          userId,
+          {
+            socketId: socket.id,   
+            status: 'active'
+          }
+        );
+      }
+    });
+
+    socket.on('update-location-captain', async (data) => {
+      const { userId, location } = data || {};
+
+      if (!userId || !location?.ltd || !location?.lng) return;
+
+      await captainModel.findByIdAndUpdate(userId, {
+        location: {
+          ltd: location.ltd,
+          lng: location.lng
         }
+      });
     });
 
-    io.on('connection', (socket) => {
-        console.log(`Client connected: ${socket.id}`);
+   
+    socket.on('disconnect', async () => {
+      console.log(`Client disconnected: ${socket.id}`);
 
-
-        socket.on('join', async (data) => {  // it is an event
-            const { userId, userType } = data;
-
-            if (userType === 'user') {
-                await userModel.findByIdAndUpdate(userId, { socketId: socket.id });
-            } else if (userType === 'captain') {
-                await captainModel.findByIdAndUpdate(userId, { socketId: socket.id });
-            }
-        });
-
-
-        socket.on('update-location-captain', async (data) => {
-            const { userId, location } = data;
-
-            if (!location || !location.ltd || !location.lng) {
-                return socket.emit('error', { message: 'Invalid location data' });
-            }
-
-            await captainModel.findByIdAndUpdate(userId, {
-                location: {
-                    ltd: location.ltd,
-                    lng: location.lng
-                }
-            });
-        });
-
-        socket.on('disconnect', () => {
-            console.log(`Client disconnected: ${socket.id}`);
-        });
+      await captainModel.updateMany(
+        { socketId: socket.id },
+        { socketId: null, status: 'inactive' }
+      );
     });
+  });
 }
+
 
 const sendMessageToSocketId = (socketId, messageObject) => {
+  console.log(messageObject);
 
-console.log(messageObject);
-
-    if (io) {
-        io.to(socketId).emit(messageObject.event, messageObject.data);
-    } else {
-        console.log('Socket.io not initialized.');
-    }
-}
+  if (io && socketId) {
+    io.to(socketId).emit(messageObject.event, messageObject.data);
+  }
+};
 
 module.exports = { initializeSocket, sendMessageToSocketId };
