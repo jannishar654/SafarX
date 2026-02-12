d7beebc381b7274c6724e3652738d05e5abd2487
Jannishar <mdjannishar654@gmail.com>
2026-02-13 00:21:22 +0530

    Live Tracking completed

--- frontend/src/pages/CaptainRiding.jsx
+++ frontend/src/pages/CaptainRiding.jsx
@@ -29,19 +29,16 @@ const CaptainRiding = () => {
   }, [finishRidePanel])
 
   return (
-   <div className='h-screen'>
+   <div className='h-screen relative flex flex-col justify-end '>
       <div className='fixed p-6 top-0 flex items-center justify-between w-screen'>
-        {/* <img className="w-16 " src="/HomeLogo.png" alt="Home Logo" /> */}
-        <LiveTracking />
-        <Link  to='/home' className='  h-10 w-4 bg-white flex items-center justify-center rounded-full '>
+        <img className="w-16 " src="/HomeLogo.png" alt="Home Logo" />
+       
+        <Link  to='/captain-home' className='  h-10 w-4 bg-white flex items-center justify-center rounded-full '>
           <i className=' text-lg font-medium ri-logout-box-r-line' ></i>
         </Link>
       </div>
 
-      <div className="h-4/5">
-        <img className='h-full w-full object-cover ' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="Home Page" />
-
-      </div>
+      
 
       <div className="h-1/5 p-6 flex items-center justify-between bg-yellow-400 relative"
       onClick={ () =>{
@@ -66,6 +63,11 @@ const CaptainRiding = () => {
       setFinishRidePanel={setFinshRidePanel}
        />
      </div>
+
+     <div className="h-screen fixed w-screen top-0  z-[-1]">
+        {/* <img className='h-full w-full object-cover ' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="Home Page" /> */}
+         <LiveTracking />
+      </div>
 
       
 
